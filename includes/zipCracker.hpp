76e0108620eca2186e4969585873c48008258989
louis <louis@jurczyk.fr>
2019-03-03 06:48:52 -0800

    add zip argument

---
+++ includes/zipCracker.hpp
@@ -0,0 +1,14 @@
+#ifndef         _ZIPCRACKER_HPP_
+#define         _ZIPCRACKER_HPP_
+
+
+class           zipCracker
+{
+    public:
+        zipCracker();
+        ~zipCracker();
+
+
+};
+
+#endif      /*_ZIPCRACKER_HPP_*/
