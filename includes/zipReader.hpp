9ea5dd00587288776cb11ee8dbea3ae1700a9fa8
louis <louis@jurczyk.fr>
2019-03-03 08:36:27 -0800

    zipreader.cpp|.hpp

---
+++ includes/zipReader.hpp
@@ -0,0 +1,10 @@
+#ifndef         _ZIPREADER_HPP_
+#define         _ZIPREADER_HPP_
+
+#include		<iostream>
+
+namespace   zipReader {
+    void    toto();
+}
+
+#endif      /*_ZIPREADER_HPP_*/
---
+++ sources/zipReader.cpp
@@ -0,0 +1,7 @@
+#include	"zipReader.hpp"
+
+namespace   zipReader {
+    void    toto(){
+    	std::cout << "bite" << std::endl;
+    }
+}
